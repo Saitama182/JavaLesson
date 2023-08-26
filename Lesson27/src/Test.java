83d3fc95c1271272c9130a65fd28ad25a9a1484f
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-26 13:49:10 +0300

    Пакет

---
+++ Lesson27/src/Test.java
@@ -0,0 +1,12 @@
+import Forest.*;
+import Forest.Something.*;
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        Tree tree1 = new Tree();
+        Squirell squirell1 = new Squirell();
+        SomeClass someClass1 = new SomeClass();
+    }
+}
