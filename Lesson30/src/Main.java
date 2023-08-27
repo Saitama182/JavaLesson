4dbffdcc72b02b766ece018e264745be21a31662
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-27 11:08:05 +0300

    Приведение примитивных типов данных

---
+++ Lesson30/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        float f = 323.44F;
+        long l = 4457L;
+
+        int x = (int)l;
+
+        double y = 123.5;
+        int b = (int)y;
+        System.out.println(b);
+
+
+    }
+}
