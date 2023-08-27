e1a8463b402b5afeadb5f06f6921b2cb86bd0987
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-27 20:30:27 +0300

    Классы-обертки примитивных типов данных

---
+++ Lesson31/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+        Integer x2 = new Integer(123);
+        Integer.parseInt("234");
+        Integer x2 = 123;
+        int y = x2;
+
+        
+
+
+    }
+}
