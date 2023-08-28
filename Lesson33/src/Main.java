f067bf8eea8926737349e2ae2fb38358f5ca3cc0
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-28 19:08:32 +0300

    Введение в параметризацию. (Generics)

---
+++ Lesson33/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> animals = new ArrayList<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Bear");
+
+        String animal = animals.get(1);
+    }
+}
