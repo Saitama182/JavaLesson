1782f01a4d302dd2e4f3d125c5ef73c8253fa7af
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-31 22:47:13 +0300

    Task

---
+++ Task1/src/Task5.java
@@ -0,0 +1,11 @@
+// Повторить задание 4 используя цикл for
+
+public class Task5 {
+    public static void main(String[] args) {
+        int age = 33;
+        for (int i = age; i < 100; i+=5) {
+            System.out.println("Мой возраст  " + i);
+
+        }
+    }
+}
