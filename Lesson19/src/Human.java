9300790e55fe630a8a9f1b8524e286c135de8667
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-06 21:37:26 +0300

    Ключевое слово this

---
+++ Lesson19/src/Human.java
@@ -0,0 +1,20 @@
+public class Human {
+    String name;
+    int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ Lesson19/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        human1.setName("Jack");
+        human1.setAge(12);
+    }
+}
