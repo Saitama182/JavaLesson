0e7727675255bf4026765fe32c3b6e6da6e636d8
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-14 20:03:24 +0300

    Строки(String) в Java. Ссылочные типы данных

---
+++ Lesson3/src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String string = "Hello";
+        String space = " ";
+        String name = "Bob";
+        System.out.println(string + space + name);
+        System.out.println("fghnftjh" + "121243214");
+
+    }
+}
