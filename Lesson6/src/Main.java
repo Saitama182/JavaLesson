6fa479090201dd1468ba82355544ec281cd71790
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-14 20:15:34 +0300

    Условный оператор if

---
+++ Lesson6/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        if (5 < 5 ){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
