b33187e0a05612d8f694a6c6c0be9e7d91674adb
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-14 20:09:34 +0300

    Цикл while

---
+++ Lesson4/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int value = 0;
+        while (value < 5 ) {
+            System.out.println("Hello " + value);
+            value +=1;
+        }
+    }
+}
