07ec930cbca1287cdc410db7a0e71019642464b1
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-14 19:53:24 +0300

    Переменные. Примитивные типы данных

---
+++ Lesson2/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 555;
+        System.out.println(myInt);
+        int myInt2 = 444;
+        short myShort = 3266;
+        long myLong =56756754;
+
+        double myDouble = 233.44;
+        float myFloat = 234.5f;
+        char c = 'a';
+        boolean b = true;
+        byte myByte = 100; 
+
+
+    }
+}
