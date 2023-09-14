c0652d3aaf5d47a3223da648c6f47cb6abc938c7
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-14 20:47:32 +0300

    Массивы в Java

---
+++ Lesson11/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[6];
+        // numbers[1] = 66;
+        for (int i=0;i< numbers.length;i++ ) {
+            numbers[i] = i*10;
+            System.out.println(numbers[i]);
+        }
+            int[] numbers1 = {1,2,3,4};
+
+
+    }
+}
