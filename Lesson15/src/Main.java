4d714fdd566351ee3b309356cb886eaf51d42d5b
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-15 21:56:45 +0300

    Методы в Java

---
+++ Lesson15/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Bob";
+        person1.age = 22;
+        person1.speak();
+    }
+}
---
+++ Lesson15/src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    String name;
+    int age;
+
+    void speak() {
+        System.out.println("My name is " + name + " my age " + age);
+    }
+}
