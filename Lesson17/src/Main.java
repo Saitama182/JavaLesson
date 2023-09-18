9f955a36d7e82062cdb2d2a7711b988f7a3399ce
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-18 20:56:22 +0300

    Параметры метода

---
+++ Lesson17/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setName("Bob");
+        person1.speak();
+    }
+}
---
+++ Lesson17/src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    int age;
+
+    void setName(String username){
+        name = username;
+    }
+    void speak(){
+        System.out.println("My name is " + name);
+    }
+}
