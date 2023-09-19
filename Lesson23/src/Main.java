00288e645b725dd697984809174f39ec86edaac2
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-19 19:06:05 +0300

    StringBuilder

---
+++ Lesson23/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my").append(" fiend");
+        System.out.println(sb.toString());
+    }
+}
