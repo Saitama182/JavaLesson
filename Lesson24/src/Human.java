cd686b95c17b4755afaf708092fb0d510dc589f1
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-19 19:27:02 +0300

    Класс Object и метод toString()

---
+++ Lesson24/src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    String name;
+    int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " " + age;
+    }
+}
---
+++ Lesson24/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Human hum1 = new Human("Bob", 12);
+        System.out.println(hum1);
+    }
+}
