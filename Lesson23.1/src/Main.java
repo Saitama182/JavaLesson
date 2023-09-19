2038a45ea0d74ece87517863fe0fb4d3d981c9f5
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-09-19 19:16:35 +0300

    Форматирование строк в Java

---
+++ Lesson23.1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%fThis is %s a string, %s\n", 10.4, "NICE", 1000);
+        System.out.printf("String %10d\n", 5476774);
+        System.out.printf("String %10d\n", 4);
+        System.out.printf("String %10d\n", 5774);
+
+    }
+}
