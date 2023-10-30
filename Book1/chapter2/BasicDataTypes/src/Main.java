2b96acd8eabcf779b4bf69a3089e214391527dd8
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-10-30 21:50:01 +0300

    instanceof

--- Book1/chapter2/BasicDataTypes/src/Main.java
+++ Book1/chapter2/BasicDataTypes/src/Main.java
@@ -5,8 +5,23 @@ public class Main {
         int c = 4_200_000;
         double d = 0.11D;
         float e = 0.123F;
+        int number = 42;
+        byte value = (byte) number;
 
+        var counter = 1;
+        System.out.println(counter);
 
-
+        int b1 = 0b1110;
+        System.out.println(b1);
+        int b2 = 0b1001;
+        System.out.println(b2);
+        int i =0;
+        System.out.println(b1 + "|" + b2 + " = " + (b1|b2));
+        System.out.println(b1 + "&" + b2 + " = " + (b1&b2));
+        System.out.println( "~" + b2 + " = " + ~b2);
+        System.out.println( b1 + ">>" + ++i + " = " + (b1>>i));
+        System.out.println( b1 + "<<" + i + " = " + (b1<<i++));
+        System.out.println( b1 + ">>>" + i + " = " + (b1>>>i));
+        
     }
 }
